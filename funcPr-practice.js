4e4d43531f737b1295ab3d99c1fae73338610f56
rnjsrnjs <zxkj12345@naver.com>
2024-11-18 17:52:20 +0900

    Create funcPr-practice.js

---
+++ funcPr-practice.js
@@ -0,0 +1,41 @@
+const cart = [
+    { item: "notebook", price: 1200000, quantity: 1 },
+    { item: "mouse", price: 35000, quantity: 2 }, 
+    { item: "keyboard", price: 89000, quantity: 1 } 
+];
+
+let totalPrice =0;
+for(let i=0; i<cart.length; i++){
+    totalPrice = cart[i].price*cart[i].quantity;}
+    console.log(`total price = ${totalPrice}`);
+totalPrice = 0;
+
+cart.forEach(a=>{
+    totalPrice += goods.price*goods.quantity;
+}
+);
+console.log(`total price = ${totalPrice}`);
+
+totalPrice =  cart.reduce((sum,goods)=>sum+goods.price*goods.quantity,0
+
+);
+console.log(`toal price =${totalPrice}`);
+
+const itemTotals = cart.map(goods =>({
+    item: goods.item,total:goods.price*goods.quantity
+}));
+
+console.log(`total price`=, itemTotals);
+
+function a(){
+    return{name:'kms',aff:'hansung'};
+}
+console.log(a());
+
+const names = ['alice', 'bob', 'charlie'];
+const uppercasedNames = names.map(n=>n.toUpperCase()
+);
+console.log(`upper cased names: ${uppercasedNames}`);
+
+const capitalStartnames = namse.map( n=>n[0].toUpperCase() + n.slice(1));
+console.log(`capital start names: ${capitalStartnames}`);
